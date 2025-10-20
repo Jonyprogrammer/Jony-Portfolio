233177a53dadbf61573a6f52596c233e18f5c978
Jonyprogrammer <jonibekhusndinov10@gmail.com>
2025-10-20 19:06:28 +0500

    Yangilandi portfolio

---
+++ Readme.txt
@@ -0,0 +1,48 @@
+🧑‍💻 Portfolio – UzJony
+👋 Salom!
+
+Men FullStack dasturchi, kiberxavfsizlik mutaxassisi va freelancerman. Ushbu portfolio mening loyihalarim, tajribam va texnik ko‘nikmalarimni namoyish etadi.
+
+💡 Soha yo‘nalishlarim:
+
+Front-end va Back-end web dasturlash
+
+Penetration Testing (Pentest)
+
+Telegram botlar yaratish
+
+Microsoft Office dasturlari bilan avtomatlashtirish
+
+⚙️ Foydalanadigan texnologiyalar:
+
+Front-end: HTML, CSS, JavaScript
+
+Back-end: PHP, Laravel
+
+Database: MySQL, JSON
+
+Security: Pentest, Vulnerability Analysis
+
+Automation: Python, Telegram Bot API
+
+🧩 Xizmatlar:
+
+Web sayt va tizimlar yaratish
+
+Xavfsizlik tahlili va testlari
+
+Botlar va avtomatlashtirilgan yechimlar
+
+Office dasturlarida professional hujjatlar va taqdimotlar tayyorlash
+
+🎯 Maqsadim:
+
+Texnologiyalar orqali foydalanuvchilarga ishonchli, xavfsiz va innovatsion yechimlar taqdim etish.
+
+📬 Bog‘lanish:
+
+Telegram: https://t.me/Jony_programmer
+
+Email: jonibekhusnidinov01@gmail.com
+
+GitHub: @Jonyprogrammer
---
+++ assets/js/main.js
@@ -0,0 +1,275 @@
+/**
+* Template Name: iPortfolio
+* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+* Updated: Jun 14 2024 with Bootstrap v5.3.3
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function() {
+  "use strict";
+
+  /**
+   * Header toggle
+   */
+  const headerToggleBtn = document.querySelector('.header-toggle');
+
+  function headerToggle() {
+    document.querySelector('#header').classList.toggle('header-show');
+    headerToggleBtn.classList.toggle('bi-list');
+    headerToggleBtn.classList.toggle('bi-x');
+  }
+  headerToggleBtn.addEventListener('click', headerToggle);
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navmenu a').forEach(navmenu => {
+    navmenu.addEventListener('click', () => {
+      if (document.querySelector('.header-show')) {
+        headerToggle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle('active');
+      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+      e.stopImmediatePropagation();
+    });
+  });
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
+   * Init typed.js
+   */
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+
+  /**
+   * Animate the skills items on reveal
+   */
+  let skillsAnimation = document.querySelectorAll('.skills-animation');
+  skillsAnimation.forEach((item) => {
+    new Waypoint({
+      element: item,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = item.querySelectorAll('.progress .progress-bar');
+        progress.forEach(el => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%';
+        });
+      }
+    });
+  });
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Init isotope layout and filters
+   */
+  document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
+    let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
+    let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
+    let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+
+    let initIsotope;
+    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
+      initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
+        itemSelector: '.isotope-item',
+        layoutMode: layout,
+        filter: filter,
+        sortBy: sort
+      });
+    });
+
+    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
+      filters.addEventListener('click', function() {
+        isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+        this.classList.add('filter-active');
+        initIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        if (typeof aosInit === 'function') {
+          aosInit();
+        }
+      }, false);
+    });
+
+  });
+
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
+
+  /**
+   * Correct scrolling position upon page load for URLs containing hash links.
+   */
+  window.addEventListener('load', function(e) {
+    if (window.location.hash) {
+      if (document.querySelector(window.location.hash)) {
+        setTimeout(() => {
+          let section = document.querySelector(window.location.hash);
+          let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+          window.scrollTo({
+            top: section.offsetTop - parseInt(scrollMarginTop),
+            behavior: 'smooth'
+          });
+        }, 100);
+      }
+    }
+  });
+
+  /**
+   * Navmenu Scrollspy
+   */
+  let navmenulinks = document.querySelectorAll('.navmenu a');
+
+  function navmenuScrollspy() {
+    navmenulinks.forEach(navmenulink => {
+      if (!navmenulink.hash) return;
+      let section = document.querySelector(navmenulink.hash);
+      if (!section) return;
+      let position = window.scrollY + 200;
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
+        navmenulink.classList.add('active');
+      } else {
+        navmenulink.classList.remove('active');
+      }
+    })
+  }
+  window.addEventListener('load', navmenuScrollspy);
+  document.addEventListener('scroll', navmenuScrollspy);
+
+  // Send Message
+  // Bot tokenini va chat ID ni o'zgaruvchilar sifatida belgilaymiz
+  const botToken = '8046315970:AAHVWWsIzIvUSSi0yITcuE8bFaKtB8PupHA';  // Bu yerga o'zingizning bot tokeningizni qo'ying
+  const chatId = '@JonyPortfolioDataBase';      // Bu yerga o'zingizning chat ID ni qo'ying
+
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Formaning standart yuborilishini to'xtatish
+
+    // Form elementlarini olish
+    const name = document.getElementById('name-field').value;
+    const email = document.getElementById('email-field').value;
+    const subject = document.getElementById('subject-field').value;
+    const message = document.getElementById('message-field').value;
+
+    // Loading va Error ko'rsatish
+    document.querySelector('.loading').style.display = 'block';
+    document.querySelector('.error-message').style.display = 'none';
+    document.querySelector('.sent-message').style.display = 'none';
+
+    // Fetch so'rovini yuborish
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: `🤖Ism: ${name}\n📧Email: ${email}\n📜Mavzu: ${subject}\n📌Izoh: ${message}`
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Agar muvaffaqiyatli yuborilsa
+      document.querySelector('.loading').style.display = 'none';
+      document.querySelector('.sent-message').style.display = 'block';
+      
+      document.getElementById('contact-form').reset();
+    })
+
+    .catch(error => {
+      // Agar xato yuz bersa
+      document.querySelector('.loading').style.display = 'none';
+      document.querySelector('.error-message').textContent = 'Xatolik yuz berdi. Iltimos, qayta urinib ko\'ring.';
+      document.querySelector('.error-message').style.display = 'block';
+    });
+  });
+})();
